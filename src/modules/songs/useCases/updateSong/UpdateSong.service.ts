a483dc48833d04db0e375537afe90e51e5bf57ed
Juliana Guimarães <jujucg15@gmail.com>
2022-11-11 19:11:24 -0300

    Criação do módulo song

---
+++ src/modules/songs/useCases/updateSong/UpdateSong.service.ts
@@ -0,0 +1,36 @@
+import { Song } from "@prisma/client";
+import { prisma } from "../../../../infra/prisma/client";
+import { IUpdateSongDTO } from "../../dtos/IUpdateSongDTO";
+
+class UpdateSongService {
+    // eslint-disable-next-line class-methods-use-this
+    async execute ({ 
+        id, 
+        name,
+        album,
+        artist 
+    }: IUpdateSongDTO): Promise<Song> {
+        const song = await prisma.song.update({ 
+            where: {
+                id: Number(id)
+            },
+            data: {
+                name: String(name),
+                album: {
+                    update: {
+                       id: Number(id)
+                    }
+                },
+                artist: {
+                    update: {
+                       id: Number(id)
+                    }
+                }
+            }
+        });
+
+        return song;
+    }
+}
+
+export default UpdateSongService;
