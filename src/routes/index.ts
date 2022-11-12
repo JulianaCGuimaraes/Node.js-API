5fd7fbe8f1c6eeb787abe5b11728ddfa637bbda4
Juliana Guimarães <jujucg15@gmail.com>
2022-11-12 16:50:34 -0300

    Criação das rotas do módulo song e palylist

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import albumRoutes from './album.routes';
 import artistRoutes from './artist.routes';
+import playlistRoutes from './playlist.routes';
 import songRoutes from './song.routes';
 import userRoutes from './user.routes';
 
@@ -10,5 +11,6 @@ routes.use('/users', userRoutes);
 routes.use('/songs', songRoutes);
 routes.use('/artists', artistRoutes);
 routes.use('/albums', albumRoutes);
+routes.use('/playlists', playlistRoutes);
 
 export default routes;
