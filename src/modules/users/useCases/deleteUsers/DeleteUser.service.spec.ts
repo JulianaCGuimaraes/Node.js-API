45a8b221a6bde89a1de780ce4a8efcbb9d7749ab
Juliana Guimarães <jujucg15@gmail.com>
2022-11-12 15:40:45 -0300

    Ajuste no teste do módulo user

--- src/modules/users/useCases/deleteUsers/DeleteUser.service.spec.ts
+++ src/modules/users/useCases/deleteUsers/DeleteUser.service.spec.ts
@@ -2,12 +2,12 @@ import DeleteUserService from "./DeleteUser.service";
 
 let deleteUser: DeleteUserService;
 
-describe('Create user', () => {
+describe('Delete user', () => {
     beforeAll(async () => {
         deleteUser = new DeleteUserService();
     });
 
-    it('should be able to create a new user', async () => {
+    it('should be able to delete user', async () => {
         const user = await deleteUser.execute({
             id: 1
         });
