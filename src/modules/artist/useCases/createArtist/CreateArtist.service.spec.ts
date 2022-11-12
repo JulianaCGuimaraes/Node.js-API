b044687bfadea8e960fb1bb7fba722acce1d894f
Juliana Guimarães <jujucg15@gmail.com>
2022-11-12 18:07:06 -0300

    Ajustes no teste do módulo artist

--- src/modules/artist/useCases/createArtist/CreateArtist.service.spec.ts
+++ src/modules/artist/useCases/createArtist/CreateArtist.service.spec.ts
@@ -10,7 +10,10 @@ describe('Create artist', () => {
     it('should be able to create a new artist', async () => {
         const artist = await createArtist.execute({
             id: 1,
-            name: 'Sinara'
+            name: 'BTS',
+            album: {
+                id: 1
+            }
         });
     
     expect(artist).toHaveProperty('id');
